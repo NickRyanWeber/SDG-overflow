3f8be7c11ae930a461e6be0b8e763109dd4beb12
nickryanweber <nicholasryanweber@gmail.com>
2019-09-25 17:44:34 -0400

    organized router and made page folder

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
 import { Layout } from './components/Layout'
-import { Home } from './components/Home'
-import { FetchData } from './components/FetchData'
-import { Counter } from './components/Counter'
+import { Home } from './pages/Home'
+import { FetchData } from './pages/FetchData'
+import { Counter } from './pages/Counter'
 import Testing from './Testing'
 
 export default class App extends Component {
